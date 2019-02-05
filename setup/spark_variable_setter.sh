afc42d34f3e3d3b9c1ca53546a0d69e60c861387
Srivastava <Srivastava.Nitin@bcg.com>
2019-02-04 22:15:38 -0800

    Revert "adding pyspark setup and kernel"
    This reverts commit a1357f12384b864b1365a2e1d4f95526e992fe90.

--- setup/spark_variable_setter.sh
+++
@@ -1,6 +0,0 @@
-echo "SPARK_HOME=/home/nsrivas3/spark-downloads/spark-2.4.0-bin-hadoop2.7" >> /home/nsrivas3/.bashrc
-echo "HADOOP_HOME=/home/nsrivas3/spark-downloads/hadoop-2.7.7" >> /home/nsrivas3/.bashrc
-echo "PYTHONPATH=/home/nsrivas3/spark-downloads/spark-2.4.0-bin-hadoop2.7/python:$PYTHONPATH" >> /home/nsrivas3/.bashrc
-echo "PYTHONPATH=/home/nsrivas3/spark-downloads/spark-2.4.0-bin-hadoop2.7/python/lib/py4j-0.10.7-src.zip:$PYTHONPATH" >> /home/nsrivas3/.bashrc
-echo "JAVA_HOME=/usr/lib/jvm/default-java/bin/java" >> /home/nsrivas3/.bashrc
-
